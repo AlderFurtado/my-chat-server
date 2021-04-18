6ab3aeadf65927b1d98e55a6d49f0bd40c78d895
AlderFurtado <aldermarcel26@outlook.com>
2021-04-17 22:19:40 -0300

    nodemon add

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ let app = express();
 app.use(express.static("."));
 
 app.get("/", (req, res) => {
-  res.json({ hello: "dss" });
+  res.json({ hello: "aaa" });
 });
 
-app.listen("3000", () => {
+app.listen("3002", () => {
   console.log("Server is listening on port 3000");
 });
