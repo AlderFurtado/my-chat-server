ec4049238b4515a20534288802438f5b7ff44848
AlderFurtado <aldermarcel26@outlook.com>
2021-04-18 00:42:24 -0300

    first commit

--- server/index.js
+++ server/index.js
@@ -8,17 +8,35 @@ app.get("/", (req, res) => {
   res.json({ hello: "world" });
 });
 
-const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer, {
-  // ...
+const httpServer = require("http");
+const server = httpServer.createServer(app);
+
+const socketIO = require("socket.io");
+
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
+const players = [];
+
 io.on("connection", (socket) => {
-  socket.on("connect", () => {
-    console.log("connectado");
+  console.log("client connected");
+
+  socket.on("logIn", () => {
+    console.log(`player ${socket.id} is connected`);
+    players.push(socket.id);
+    console.log(players);
+    socket.emit("allPlayers", players);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
-app.listen("3002", () => {
+server.listen("3002", () => {
   console.log("Server is listening on port 3002");
 });
