28800ae023de6412a6b2058a7d8c2b0066fe9025
AlderFurtado <aldermarcel26@outlook.com>
2021-04-17 21:10:47 -0300

    add connection backend to frontend

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 let app = express();
+const cors = require("cors");
 
 app.use(express.static("."));
 
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.json({ hello: "aaa" });
+  res.json({ hello: "world" });
 });
 
 app.listen("3002", () => {
-  console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3002");
 });
